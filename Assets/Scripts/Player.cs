bd568a2de64ef47894556e300baf8271b8eb1d40
alex-rafael-13 <rafael.alex1398@gmail.com>
2023-08-31 23:55:45 -0700

    bird movement by inputs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private Vector3 direction;
+    
+    public float gravity = -9.8f;
+    public float strength = 5f;
 
-    // Update is called once per frame
-    void Update()
+    private void Update() //Inputs
+    // Space bar or left click flaps the bird
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)){
+            direction = Vector3.up * strength;
+        }
+        //Touch screen inputs
+        if (Input.touchCount > 0){ //Checks if the screen got touched at least once
+            Touch touch = Input.GetTouch(0); //Gets first touch, disregarding the others
+
+            if(touch.phase == TouchPhase.Began){ //Checks if touch just began
+                direction = Vector3.up * strength;
+            }
+        }
+
+        direction.y += gravity * Time.deltaTime;
+        transform.position += direction * Time.deltaTime; //Framerate independent
     }
 }
