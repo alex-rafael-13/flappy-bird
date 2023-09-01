0e82bb1e5d02b424ef4e9269b5dbd650893fbfc9
alex-rafael-13 <rafael.alex1398@gmail.com>
2023-09-01 11:37:59 -0700

    working on pipe spawning

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject prefab;
+    public float spawnRate = 1f;
+    public float minHeight = -1f;
+    public float maxHeight = 1f;
+
+    private void OnEnable()
+    {
+        InvokeRepeating(nameof(Spawn), spawnRate, spawnRate);
+    }
+
+    private void OnDisable()
+    {
+        CancelInvoke(nameof(Spawn))
+    }
+}
