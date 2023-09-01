0fd79900d3e1baf073675d03769c7f8666f978f0
alex-rafael-13 <rafael.alex1398@gmail.com>
2023-09-01 16:26:13 -0700

    pipes spawning

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -18,6 +18,10 @@ public class Spawner : MonoBehaviour
     }
     private void Spawn()
     {
+        //Instantiate is used to create new instances of game objects, ususally used with prefabs
+        //Quartation is data type that is used to represent rotation in 3D spaces. Quartation.identity is predefined constant that reps neutral
         GameObject pipes = Instantiate(prefab, transform.position, Quaternion.identity);
+        //Random value created is kept between min max values, then multiplied by the x, y, and z from the Vector3
+        pipes.transform.position += Vector3.up * Random.Range(minHeight, maxHeight); //Vector3.up has a preset of (0, 1, 0) 
     }
 }
