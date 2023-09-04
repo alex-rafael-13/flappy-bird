9e82aceb2f91ece44245f13f0ceaf39364f302e2
alex-rafael-13 <rafael.alex1398@gmail.com>
2023-09-03 19:49:54 -0700

    added pipes spawner

--- Assets/Scripts/Pipes.cs
+++ Assets/Scripts/Pipes.cs
@@ -6,8 +6,16 @@ public class Pipes : MonoBehaviour
 {
     public float speed = 5f;
 
+    private float leftEdge;
+
     private void Update()
     {
+        //Moves the pipes across the screen
         transform.position += Vector3.left * speed * Time.deltaTime;
+
+        //Removing pipes at the end of the screen
+        // if(transform.position.x > leftEdge){
+        //     Destroy(gameObject);
+        // }
     }
 }
